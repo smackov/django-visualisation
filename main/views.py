52ddce9bed3ec99f880b4dd3ac6c6452c52322d8
smackov <smackovdinislam@gmail.com>
2020-11-02 22:56:18 +0500

    New classes 'UserTracks' and ' 'Dates' were added to service.py

--- main/views.py
+++ main/views.py
@@ -3,39 +3,50 @@ from django.contrib.auth.decorators import login_required
 from .form import TaskForm, TrackForm, RateForm
 from .models import Task, Track, Rate
 from .services import (
-    get_week_information, 
+    get_week_information,
     get_last_four_weeks,
     # get_statistic,
+    UserTracks,
 )
 
 from datetime import date
 
+
 def date_today():
     return date.today().strftime('%A %d %B')
+
 
 def number_day():
     return date.today().toordinal() - date(date.today().year, 1, 1).toordinal() + 1
 
 
 def index(request):
+
     context = {
-            'date': date_today(),
-            'number_day': number_day(),
-        }
+        'date': date_today(),
+        'number_day': number_day(),
+    }
+
     if request.user.is_authenticated:
+        userTracks = UserTracks(request.user)
         context = {
             'date': date_today(),
             'number_day': number_day(),
             'week_data': get_week_information(request.user),
             'last_weeks': get_last_four_weeks(request.user),
-            # 'statistic': get_work_time_statistic(request.user),
+            'statistic': {
+                'current_week': userTracks.work_time_current_week(),
+                'current_month': userTracks.work_time_current_month(),
+            },
         }
+
     return render(request, 'main/index.html', context)
+
 
 @login_required
 def add_task(request):
     if request.method == 'POST':
-        form = TaskForm(request.POST) 
+        form = TaskForm(request.POST)
         if form.is_valid():
             task = form.save(commit=False)
             task.author = request.user
@@ -49,6 +60,7 @@ def add_task(request):
         'number_day': number_day(),
     }
     return render(request, 'main/add_task.html', context)
+
 
 @login_required
 def add_track(request):
