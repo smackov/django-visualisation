d41b555c124e3844eda4be99d3e657753119e35b
smackov <smackovdinislam@gmail.com>
2020-11-20 19:39:20 +0500

    models.py was styled according to PEP8

--- main/models.py
+++ main/models.py
@@ -2,9 +2,12 @@ from django.db import models
 from django.conf import settings
 
 # Create your models here.
+
+
 class Task(models.Model):
     name = models.CharField(max_length=40)
-    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    author = models.ForeignKey(settings.AUTH_USER_MODEL,
+                               on_delete=models.CASCADE)
 
     def __str__(self):
         return self.name
@@ -20,19 +23,20 @@ class Rate(models.Model):
     def __str__(self):
         return self.name
 
-    
+
 class Track(models.Model):
     date = models.DateField()
-    id_task = models.ForeignKey("Task", on_delete=models.CASCADE, verbose_name='Task')
+    id_task = models.ForeignKey("Task", on_delete=models.CASCADE,
+                                verbose_name='Task')
     duration = models.IntegerField()
-    id_rate = models.ForeignKey("Rate", on_delete=models.CASCADE, verbose_name='Rate')
-    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    id_rate = models.ForeignKey("Rate", on_delete=models.CASCADE,
+                                verbose_name='Rate')
+    author = models.ForeignKey(settings.AUTH_USER_MODEL,
+                               on_delete=models.CASCADE)
 
     class Meta:
         ordering = ('date', 'author', 'id_task')
 
     def __str__(self):
-        return f'{self.id} - ' + self.id_task.name + f' - {self.duration} pom-ro' + f' - {self.date}'
-
-
-
+        return (f'{self.id} - ' + self.id_task.name +
+                f' - {self.duration} pom-ro' + f' - {self.date}')
