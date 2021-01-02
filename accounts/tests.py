98b3bde62f8da98e5129da463e107f3e75a7380d
smackov <smackovdinislam@gmail.com>
2021-01-02 23:34:39 +0500

    Add signup tests

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,17 +1,28 @@
 from django.urls import resolve, reverse
 from django.test import TestCase
+from django.contrib.auth.forms import UserCreationForm
 
 from .views import signup
 
 
 class SignUpTests(TestCase):
     "Test signup view of this application"
-    def test_signup_status_code(self):
+    
+    def setUp(self):
         url = reverse('signup')
-        response = self.client.get(url)
-        self.assertEquals(response.status_code, 200)
+        self.response = self.client.get(url)
+        
+    def test_signup_status_code(self):
+        self.assertEquals(self.response.status_code, 200)
 
     def test_signup_url_resolves_signup_view(self):
         view = resolve('/signup/')
         self.assertEquals(view.func, signup)
+
+    def test_csrf(self):
+        self.assertContains(self.response, 'csrfmiddlewaretoken')
+
+    def test_contains_form(self):
+        form = self.response.context.get('form')
+        self.assertIsInstance(form, UserCreationForm)
         
