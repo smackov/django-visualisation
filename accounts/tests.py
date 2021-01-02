975b6f5d99c175b3f7388ac63d0e27caf12570b5
smackov <smackovdinislam@gmail.com>
2021-01-02 23:44:59 +0500

    Add 'SuccessfulSignUpTests' test class

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,17 +1,18 @@
 from django.urls import resolve, reverse
 from django.test import TestCase
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 
 from .views import signup
 
 
 class SignUpTests(TestCase):
     "Test signup view of this application"
-    
+
     def setUp(self):
         url = reverse('signup')
         self.response = self.client.get(url)
-        
+
     def test_signup_status_code(self):
         self.assertEquals(self.response.status_code, 200)
 
@@ -25,4 +26,34 @@ class SignUpTests(TestCase):
     def test_contains_form(self):
         form = self.response.context.get('form')
         self.assertIsInstance(form, UserCreationForm)
-        
+
+
+class SuccessfulSignUpTests(TestCase):
+    def setUp(self):
+        url = reverse('signup')
+        data = {
+            'username': 'Jane',
+            'password1': 'abcdef123456',
+            'password2': 'abcdef123456'
+        }
+        self.response = self.client.post(url, data)
+        self.index_url = reverse('index')
+
+    def test_redirection(self):
+        '''
+        A valid form submission should redirect the user to the index page
+        '''
+        self.assertRedirects(self.response, self.index_url)
+
+    def test_user_creation(self):
+        self.assertTrue(User.objects.exists())
+
+    def test_user_authentication(self):
+        '''
+        Create a new request to an arbitrary page.
+        The resulting response should now have a `user` to its context,
+        after a successful sign up.
+        '''
+        response = self.client.get(self.index_url)
+        user = response.context.get('user')
+        self.assertTrue(user.is_authenticated)
