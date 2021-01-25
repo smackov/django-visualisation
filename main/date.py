088598bb659c8b6812ecf13c19333a9a58b35d70
smackov <smackovdinislam@gmail.com>
2021-01-25 16:19:30 +0500

    Change main/Date module
    Delecte class attributes '__TODAY', '__MONTH', '__YEAR'.
    Replace them in the code by datetime.date methods

--- main/date.py
+++ main/date.py
@@ -9,23 +9,19 @@ from dateutil.relativedelta import relativedelta
 
 class Date:
     "Proccess date in needest format"
-
-    __TODAY = date.today()
-    __MONTH = __TODAY.month
-    __YEAR = __TODAY.year
     
     @classmethod
     def first_day_of_current_week(cls):
         "Find first day of a current week."
 
-        first_day = cls.__TODAY - timedelta(days=cls.__TODAY.weekday())
+        first_day = date.today() - timedelta(days=date.today().weekday())
         return first_day
 
     @classmethod
     def current_week_dates(cls):
         "Find first and last dates of a current week."
 
-        first_day = cls.__TODAY - timedelta(days=cls.__TODAY.weekday())
+        first_day = date.today() - timedelta(days=date.today().weekday())
         last_day = first_day + timedelta(days=6)
         return first_day, last_day
 
@@ -33,7 +29,7 @@ class Date:
     def last_week_dates(cls):
         "Find first and last dates of a last week."
 
-        first_day = cls.__TODAY - timedelta(days=cls.__TODAY.weekday() + 7)
+        first_day = date.today() - timedelta(days=date.today().weekday() + 7)
         last_day = first_day + timedelta(days=6)
         return first_day, last_day
 
@@ -41,8 +37,8 @@ class Date:
     def current_month_dates(cls):
         "Find first and last dates of a current month."
         
-        first_day = cls.__TODAY + relativedelta(day=1)
-        last_day = cls.__TODAY + relativedelta(day=1, months=+1, days=-1)
+        first_day = date.today() + relativedelta(day=1)
+        last_day = date.today() + relativedelta(day=1, months=+1, days=-1)
         
         return first_day, last_day
 
@@ -50,7 +46,7 @@ class Date:
     def last_month_dates(cls):
         "Find first and last dates of a last month."
         
-        first_day = cls.__TODAY + relativedelta(day=1, months=-1)
-        last_day = cls.__TODAY + relativedelta(day=1, days=-1)
+        first_day = date.today() + relativedelta(day=1, months=-1)
+        last_day = date.today() + relativedelta(day=1, days=-1)
         
         return first_day, last_day
